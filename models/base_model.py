04098b58bc72cd7bf098790cae17d1107a35da2c
Tankiso Fuma <146758442+Fuma1322@users.noreply.github.com>
2024-02-28 12:53:14 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -3,17 +3,33 @@
 """
 import uuid
 from datetime import datetime
+import models
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+    def __init__(self, *args, **kwargs):
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, time_formart))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
+
+        models.storage.new(self)
       
     def save(self):
         """
         """
         self.updated_at = datetime.utcnow()
+        models.storage.save()
+    
     def to_dict(self):
         """
         """
