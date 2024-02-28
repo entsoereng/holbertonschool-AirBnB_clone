b7231160924dad27b9c34e0b55b082ffa66207c2
Tankiso Fuma <146758442+Fuma1322@users.noreply.github.com>
2024-02-28 15:33:17 +0200

    Update console.py

--- console.py
+++ console.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+import cmd
+import shlex
+from models.base_model import BaseModel
+from models.user import User
+from model import storage
+
+class HBNBCommand(cmd.Cmd):
+  propmt "(hbnb) "
+  valid_classes = ["BaseModel", "User"]
+
+
+
+def emptyline(self):
+  """
+  Do Nothing when an empty line is entered
+  """
+  pass
+
+def do EOF(self,line):
+"""
+Hand the End-of-File (Ctrl+D) event to exit the program
+"""
+return True
