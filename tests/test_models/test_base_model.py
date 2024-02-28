a8b909331dc3d33b5c0b4ee655d5a9b07e22b2ba
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-02-28 13:04:13 +0200

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -19,7 +19,7 @@ class TestBasemodel(unittest.TestCase):
     def test_to_dict(self):
         my_model = BaseModel()
         my_model_dict = my_model.to_dict()
-        self.assertIsInstance9my_model_dict, dict)
+        self.assertIsInstance(my_model_dict, dict)
 
         self.assertEqual(my_model_dict["__class__"], 'BaseModel')
         self.assertEqual(my_model_dict['id'], my_model.id)
