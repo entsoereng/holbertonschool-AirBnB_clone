a92770057a35b9dbbf3b2668fd2da9a77a849aca
Tankiso Fuma <146758442+Fuma1322@users.noreply.github.com>
2024-02-28 12:27:20 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,54 +5,48 @@ from models.base_model import BaseModel
 
 class FileStorage:
     """
-    
+    A simple file storage mechanism for managing instances of BaseModel objects.
     """
-  __file_path = "file.json"
+    __file_path = "file.json"
+    __objects = {}
 
-  __objects = {}
+    def new(self, obj):
+        """
+        Adds a new object to the storage.
+        """
+        obj_cls_name = obj.__class__.__name__
+        key = "{}.{}".format(obj_cls_name, obj.id)
+        FileStorage.__objects[key] = obj
 
-  def new(self.obj):
-    """
+    def all(self):
+        """
+        Returns all objects in the storage.
+        """
+        return FileStorage.__objects
 
-    """
-    obj_cls_name = obj.__class__.__name__
-    key = "{}.{}".format(obj_cls_name, obj.id)
-    FileStorage.__objects[key] = obj
+    def save(self):
+        """
+        Saves all objects to a JSON file.
+        """
+        all_objs = FileStorage.__objects
+        obj_dict = {}
+        for obj_key, obj in all_objs.items():
+            obj_dict[obj_key] = obj.to_dict()
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
+            json.dump(obj_dict, file)
 
-def all(self):
-    """
-
-    """
-  return FileStorage.__objects
-
-def save(self):
-  """
-
-  """
-  all_obj = FileStrorage.__objects
-  obj_dict = {}
-  for obj in all_objs.keys():
-    obj_dict[obj] = all_objs[obj].to_dict()
-  with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
-      json.dump(obj_dict, file)
-
-  def reload(self):
-    """
-
-    """
-    if os.path.isFile(FileStorage.__file_path):
-        with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
-          try:
-              obj_dict = json.load(file)
-
-              for key, value in obj_dict.items():
-                  class_name, obj_id = key.split('.')
-                
-                  cls = eval(class_name)
-
-                  instance = cls(**values)
-
-                  FileStorage.__objects[key] = instance
-          except Exception:
-            pass
-                  
+    def reload(self):
+        """
+        Loads objects from a JSON file.
+        """
+        if os.path.isfile(FileStorage.__file_path):
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
+                try:
+                    obj_dict = json.load(file)
+                    for key, value in obj_dict.items():
+                        class_name, obj_id = key.split('.')
+                        cls = eval(class_name)
+                        instance = cls(**value)
+                        FileStorage.__objects[key] = instance
+                except Exception:
+                    pass
