7883f8aade41d22d01d7b496fbbcc67325522f90
Tankiso Fuma <146758442+Fuma1322@users.noreply.github.com>
2024-02-28 15:51:56 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -8,6 +8,10 @@ import models
 class BaseModel:
     def __init__(self, *args, **kwargs):
         time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+        
         if kwargs:
             for key, value in kwargs.items():
                 if key == "__class__":
@@ -16,11 +20,6 @@ class BaseModel:
                     setattr(self, key, datetime.strptime(value, time_format))
                 else:
                     setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
 
         models.storage.new(self)
       
