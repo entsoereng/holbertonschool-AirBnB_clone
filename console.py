6899cace63d7fabaa40838fce8ed841171100644
Tankiso Fuma <146758442+Fuma1322@users.noreply.github.com>
2024-02-28 15:40:56 +0200

    Update console.py

--- console.py
+++ console.py
@@ -18,7 +18,29 @@ def emptyline(self):
   pass
 
 def do EOF(self,line):
-"""
-Hand the End-of-File (Ctrl+D) event to exit the program
-"""
-return True
+  """
+  Hand the End-of-File (Ctrl+D) event to exit the program
+  """
+  return True
+
+def do_quit(self, arg)
+  """
+  Quit the program
+  """
+  return True
+
+def do_create(self, arg)
+  """
+  Create a new instance of BaseModel and save it to the JSON file.
+  Usage: create <class_name>
+  """
+  commands = shlex.split(arg)
+
+  if len(commands) == 0:
+    print("** class name missing**")
+  elif commands[0] not in self.valid_classes:
+    print("** class doesn't exist**")
+  else:
+      new instance = BaseModel ()
+      new_instance.save()
+      print(new_instance.id)
