2e76823b2eeda5ef0aead08135818aab1e616f8c
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-03-01 12:07:00 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """
+Module for the BaseModel class.
 """
 import uuid
 from datetime import datetime
 import models
+
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
@@ -22,15 +24,17 @@ class BaseModel:
                     setattr(self, key, value)
 
         models.storage.new(self)
-      
+
     def save(self):
         """
+
         """
         self.updated_at = datetime.utcnow()
         models.storage.save()
-    
+
     def to_dict(self):
         """
+
         """
         inst_dict = self.__dict__.copy()
         inst_dict["__class__"] = self.__class__.__name__
@@ -41,19 +45,31 @@ class BaseModel:
 
     def __str__(self):
         """
+
         """
         class_name = self.__class__.__name__
         return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
+
 if __name__ == "__main__":
     my_model = BaseModel()
-    my_model.name = "My First Model"
+    my_model.name = "My_First_Model"
     my_model.my_number = 89
+    print(my_model.id)
     print(my_model)
-    my_model.save()
-    print(my_model)
+    print(type(my_model.created_at))
+    print("--")
     my_model_json = my_model.to_dict()
     print(my_model_json)
     print("JSON of my_model:")
     for key in my_model_json.keys():
         print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+    print("--")
+    my_new_model = BaseModel(**my_model_json)
+    print(my_new_model.id)
+    print(my_new_model)
+    print(type(my_new_model.created_at))
+
+    print("--")
+    print(my_model is my_new_model)
