22cc7700dd64d0e01be9e44d2d676c6daea3f091
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-03-01 15:15:46 +0200

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -1506,6 +1506,8 @@ class TestHBNBCommand_count(unittest.TestCase):
         with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd("Review.count()"))
             self.assertEqual("1", output.getvalue().strip())
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertEqual(HBNBCommand().onecmd("help show"))
 
 
 if __name__ == "__main__":
